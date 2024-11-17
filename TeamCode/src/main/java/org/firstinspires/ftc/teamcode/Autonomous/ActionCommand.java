b27c5cda52e9171e5d5a015c978506f795dcea67
GOBI8450 <epiestun@gmail.com>
2024-11-17 19:31:20 +0200

    Still not working added actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ActionCommand.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.Subsystem;
+
+import org.apache.commons.math3.ode.events.Action;
+import org.firstinspires.ftc.teamcode.MMSystems;
+
+import java.util.Set;
+
+public class ActionCommand implements Command {
+    private final Action action;
+    private final Set<Subsystem> requirements;
+    private boolean finished = false;
+
+    public ActionCommand(Action action, Set<Subsystem> requirements) {
+        this.action = action;
+        this.requirements = requirements;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return requirements;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
+}
