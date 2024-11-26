aac3ae2b1f344fc76302c9f510caa3252279f980
GOBI8450 <epiestun@gmail.com>
2024-11-26 20:10:18 +0200

    Added RoadRunner!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ActionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ActionCommand.java
@@ -7,35 +7,34 @@ import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.Subsystem;
 
 import org.apache.commons.math3.ode.events.Action;
+import org.firstinspires.ftc.teamcode.CommandGroup.DriveCommand;
+import org.firstinspires.ftc.teamcode.CommandGroup.RobotCommands;
 import org.firstinspires.ftc.teamcode.MMSystems;
-
 import java.util.Set;
 
-public class ActionCommand implements Command {
-    private final Action action;
-    private final Set<Subsystem> requirements;
-    private boolean finished = false;
-
-    public ActionCommand(Action action, Set<Subsystem> requirements) {
-        this.action = action;
-        this.requirements = requirements;
-    }
-
-    @Override
-    public Set<Subsystem> getRequirements() {
-        return requirements;
-    }
-
-    @Override
-    public boolean isFinished() {
-        return finished;
-    }
-
-    public void run(){
-
-    }
-
-    public void preview(){
-
-    }
+public class ActionCommand {
+//    private final Action action;
+//
+//    public ActionCommand(Action action, Set<Subsystem> requirements) {
+//        this.action = action;
+//    }
+//
+//    @Override
+//    public void initialize() {
+//        action.start();
+//    }
+//
+//    @Override
+//    public void execute(){
+//        action.run();
+//    }
+//
+//    @Override
+//    public boolean isFinished(){
+//        return action.
+//    }
+//
+//    public void preview(){
+//
+//    }
 }
