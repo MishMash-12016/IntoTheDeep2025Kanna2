a154242bb1e2a54438a578af6712a9b53d4abee8
GOBI8450 <epiestun@gmail.com>
2024-12-01 19:16:12 +0200

    autoCmmand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ActionCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ActionCommand.java
@@ -5,36 +5,38 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.Subsystem;
-
-import org.apache.commons.math3.ode.events.Action;
-import org.firstinspires.ftc.teamcode.CommandGroup.DriveCommand;
 import org.firstinspires.ftc.teamcode.CommandGroup.RobotCommands;
 import org.firstinspires.ftc.teamcode.MMSystems;
 import java.util.Set;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Actions;
 
-public class ActionCommand {
-//    private final Action action;
-//
-//    public ActionCommand(Action action, Set<Subsystem> requirements) {
-//        this.action = action;
-//    }
-//
-//    @Override
-//    public void initialize() {
-//        action.start();
-//    }
-//
-//    @Override
-//    public void execute(){
-//        action.run();
-//    }
-//
-//    @Override
-//    public boolean isFinished(){
-//        return action.
-//    }
-//
-//    public void preview(){
-//
-//    }
+
+public class ActionCommand implements Command {
+    private final Action action;
+    private final Set<Subsystem> requirements;
+    private boolean finished = false;
+
+    public ActionCommand(Action action, Set<Subsystem> requirements) {
+        this.action = action;
+        this.requirements = requirements;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return requirements;
+    }
+
+    @Override
+    public void execute() {
+        TelemetryPacket packet = new TelemetryPacket();
+        action.preview(packet.fieldOverlay());
+        finished = !action.run(packet);
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
 }
